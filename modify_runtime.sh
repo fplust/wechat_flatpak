9969eb3361dc428e1a5f82d8184448cd844de4cb
fplust <fplustlu@gmail.com>
2024-03-08 18:59:20 +0800

    微信 flatpak

---
+++ modify_runtime.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+cp ./LICENSE /var/lib/flatpak/runtime/org.freedesktop.Platform/x86_64/23.08/active/files/etc/LICENSE
+cp ./.kyact /var/lib/flatpak/runtime/org.freedesktop.Platform/x86_64/23.08/active/files/etc/.kyact
+cp ./lsb-release-ukui /var/lib/flatpak/runtime/org.freedesktop.Platform/x86_64/23.08/active/files/etc/lsb-release
+cp ./libactivation.so /var/lib/flatpak/runtime/org.freedesktop.Platform/x86_64/23.08/active/files/lib/libactivation.so
